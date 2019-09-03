38021331497acb024aac60d5457f0b8afe995969
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-03 23:09:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+   counter = 0
+   sq_array = []
+  while counter < array.length do
+	   sq_array.push(array[counter] ** 2)
+	    counter += 1
+	  end
 end
